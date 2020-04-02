e781bd0c47ef117956e91715157534b735779004
Andrew <andrey.nesm57@gmail.com>
2020-04-02 12:57:02 +0300

    TODO. Add generation of ndvi and ndwi

--- research_code/run_experiment.py
+++ research_code/run_experiment.py
@@ -18,26 +18,34 @@ def find_best_model(model_dir):
     return best_model_name
 
 
+def generate_ndvi():
+    pass
+
+
+def generate_ndwi():
+    pass
+
+
 def run_experiment():
     experiment_dir, model_dir, experiment_name = train()
     prediction_dir = os.path.join(experiment_dir, "predictions")
     best_model_name = find_best_model(model_dir)
     weights_path = os.path.join(model_dir, best_model_name)
 
-    test_df = args.dataset_df
+    test_df_path = args.dataset_df
     test_data_dir = args.val_dir
     print(f"Starting prediction process. Using {best_model_name} for prediction")
     predict(output_dir=prediction_dir,
             class_names=args.class_names,
             weights_path=weights_path,
-            test_df_path=test_df,
+            test_df_path=test_df_path,
             test_data_dir=test_data_dir,
             stacked_channels=args.stacked_channels)
     print(f"Starting evaluation process of results in {prediction_dir}")
     evaluate(test_dir=test_data_dir,
              prediction_dir=prediction_dir,
              output_csv=f"{experiment_name}_img_ious.csv",
-             test_df_path=test_df,
+             test_df_path=test_df_path,
              threshold=args.threshold,
              class_names=args.class_names)
 
